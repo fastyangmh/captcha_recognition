7019c9f1ca4a26d5b06c9f8faf2e2fe3697d0195
YMH <fastyangmh@gmail.com>
2020-02-03 09:10:39 +0800

    redesign model

--- src/model.py
+++ src/model.py
@@ -5,46 +5,58 @@ import torch.nn as nn
 # def
 
 
-def calculate_dim(Hin, Win, kernel_size, padding=0, dilation=1):
-    for k in kernel_size:
-        for _ in range(2):
-            Hin = Hin+2*padding-dilation*(k-1)-1+1
-            Win = Win+2*padding-dilation*(k-1)-1+1
-    for _ in range(2):
-        Hin = Hin+2*padding-dilation*(kernel_size[-1]-1)-1+1
-        Win = Win+2*padding-dilation*(kernel_size[-1]-1)-1+1
-    return Hin, Win
+def calculate_dim(Hin, Win, kernel_size, stride=1, padding=0, dilation=1):
+    Hin = ((Hin+2*padding-dilation*(kernel_size-1)-1)/stride)+1
+    Win = ((Win+2*padding-dilation*(kernel_size-1)-1)/stride)+1
+    return int(Hin), int(Win)
 
 # class
 
 
 class DEEPCNN(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, Hin, Win, hidden_dim, n_hidden, out_dim, dropout=0.5):
+    def __init__(self, in_channels, out_channels, kernel_sizes, strides, paddings, dilations, Hin, Win, hidden_dim, n_hidden, out_dim, dropout=0.5):
         super(DEEPCNN, self).__init__()
-        self.convs = nn.ModuleList([nn.Conv2d(
-            in_channels=i, out_channels=j, kernel_size=k) for i, j, k in zip(in_channels, out_channels, kernel_size)])
-        self.pools = nn.ModuleList(
-            [nn.MaxPool2d(kernel_size=i, stride=1) for i in kernel_size])
-        self.last_conv = nn.Conv2d(
-            in_channels=out_channels[-1], out_channels=1, kernel_size=kernel_size[-1])
-        self.last_pool = nn.MaxPool2d(kernel_size=kernel_size[-1], stride=1)
-        Hout, Wout = calculate_dim(Hin, Win, kernel_size)
-        in_sizes = [Wout]+[hidden_dim]*(n_hidden-1)
+        self.convs = nn.ModuleList([nn.Conv2d(in_channels=i, out_channels=o, kernel_size=k, stride=s, padding=p, dilation=d)
+                                    for i, o, k, s, p, d in zip(in_channels, out_channels, kernel_sizes, strides, paddings, dilations)])
+        self.pools = nn.ModuleList([nn.MaxPool2d(kernel_size=k, stride=s, padding=p, dilation=d)
+                                    for k, s, p, d in zip(kernel_sizes, strides, paddings, dilations)])
+        for k, s, p, d in zip(kernel_sizes, strides, paddings, dilations):
+            Hin, Win = calculate_dim(Hin, Win, k, s, p, d)
+            Hin, Win = calculate_dim(Hin, Win, k, s, p, d)
+        in_sizes = [Win]+[hidden_dim]*(n_hidden-1)
         out_sizes = [hidden_dim]*n_hidden
         self.layers = nn.ModuleList([nn.Linear(in_size, out_size) for (
             in_size, out_size) in zip(in_sizes, out_sizes)])
-        self.last_layer = nn.Linear(
-            in_features=hidden_dim, out_features=out_dim)
+        self.last_layer = nn.Linear(hidden_dim, out_dim)
         self.leakyrelu = nn.LeakyReLU(inplace=True)
-        self.softmax = nn.Softmax(dim=2)
+        self.relu = nn.ReLU(inplace=True)
+        self.softmax = nn.Softmax(dim=3)
         self.sigmoid = nn.Sigmoid()
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         for conv, pool in zip(self.convs, self.pools):
-            x = pool(conv(x))
-        x = self.last_pool(self.last_conv(x))
+            x = self.leakyrelu(self.dropout(pool(conv(x))))
         for layer in self.layers:
-            x = self.dropout(self.leakyrelu(layer(x)))
-        x = self.softmax(self.last_layer(x))
-        return x
+            x = self.leakyrelu(self.dropout(layer(x)))
+        x = self.softmax(self.dropout(self.last_layer(x)))
+        return x.squeeze(1)
+
+
+class MLP(nn.Module):
+    def __init__(self, in_dim, out_dim, hidden_dim, n_hidden, dropout=0.5):
+        super(MLP, self).__init__()
+        in_sizes = [in_dim]+[hidden_dim]*(n_hidden-1)
+        out_sizes = [hidden_dim]*n_hidden
+        self.layers = nn.ModuleList([nn.Linear(in_size, out_size) for (
+            in_size, out_size) in zip(in_sizes, out_sizes)])
+        self.last_layer = nn.Linear(hidden_dim, out_dim)
+        self.leakyrelu = nn.LeakyReLU(inplace=True)
+        self.softmax = nn.Softmax(dim=3)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = self.leakyrelu(self.dropout(layer(x)))
+        x = self.softmax(self.dropout(self.last_layer(x)))
+        return x.squeeze(1)
--- src/train.py
+++ src/train.py
@@ -14,7 +14,6 @@ from tqdm import tqdm
 from sklearn.metrics import accuracy_score
 
 #global parameters
-RANDOM_SEED = 1234
 USE_CUDA = torch.cuda.is_available()
 
 
@@ -23,6 +22,7 @@ USE_CUDA = torch.cuda.is_available()
 
 def train_loop(dataloader, model, optimizer, criterion, epochs):
     train_loader, test_loader = dataloader
+    criterion_bce, criterion_mse = criterion
     history = []
     for epoch in tqdm(range(epochs)):
         model.train()
@@ -31,12 +31,17 @@ def train_loop(dataloader, model, optimizer, criterion, epochs):
             if USE_CUDA:
                 x, y = x.cuda(), y.cuda()
             yhat = model(x)
-            loss = criterion(yhat, y.float())
+            loss = criterion_bce(yhat, y)+criterion_mse(yhat, y)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             total_loss.append(loss.item())
         history.append(np.mean(total_loss))
+        if not epoch % 100:
+            ohe = onehotencoder()
+            _, acc = evaluation(train_loader, model, ohe)
+            print('Train accuracy:{}, loss:{}'.format(
+                round(np.mean(acc), 8), round(history[-1], 8)))
     return history
 
 
@@ -48,8 +53,9 @@ def evaluation(dataloader, model, decoder):
         if USE_CUDA:
             x, y = x.cuda(), y.cuda()
         pred = model(x).cpu().data.numpy()
-        pred = decoder.decoding(pred)
-        acc = accuracy_score(decoder.decoding(y.cpu().data.numpy()), pred)
+        pred = np.argmax(pred, 2)
+        y = torch.argmax(y, 2).cpu().data.numpy()
+        acc = accuracy_score(decoder.decoding(y), decoder.decoding(pred))
         predict.append(pred)
         accuracy.append(acc)
     return predict, accuracy
@@ -77,14 +83,15 @@ class onehotencoder:
         in order to easy training, i expand dimensions and swap axes from initial dimension.
         initial dimention is (1040,5,36)
         '''
-        return np.expand_dims(encode, 1).swapaxes(2, 3)
+        return np.array(encode)  # np.expand_dims(encode, 1).swapaxes(2, 3)
 
     def decoding(self, label):
         decode = []
         for l in label:
             code = ''
-            index = np.argmax(l, 1)
-            for i in index[0]:
+            #index = np.argmax(l, 1)
+            index = l
+            for i in index:  # [0]:
                 code += self.alphabet_dict[i]
             decode.append(code)
         return decode
@@ -93,12 +100,12 @@ class onehotencoder:
 if __name__ == "__main__":
     # parameters
     data_path = './data'
-    batch_size = 32
-    lr = 0.001
-    epochs = 50
-    torch.manual_seed(RANDOM_SEED)
+    batch_size = 128
+    lr = 0.01
+    epochs = 20
 
     # load data
+    print('load data')
     data = []
     label = []
     files = glob(join(data_path, '*.png'))
@@ -110,33 +117,38 @@ if __name__ == "__main__":
     label = np.array(label)
 
     # preprocessing
+    print('preprocessing')
     data = np.expand_dims(data/255., 1)
     ohe = onehotencoder()
     X_train, X_test, Y_train, Y_test = train_test_split(
         data, ohe.encoding(label), test_size=0.3)
     train_set = TensorDataset(torch.from_numpy(
-        X_train).float(), torch.from_numpy(Y_train))
+        X_train).float(), torch.from_numpy(Y_train).float())
     train_loader = DataLoader(dataset=train_set, batch_size=batch_size,
                               shuffle=True, num_workers=4, pin_memory=True)
     test_set = TensorDataset(torch.from_numpy(
-        X_test).float(), torch.from_numpy(Y_test))
+        X_test).float(), torch.from_numpy(Y_test).float())
     test_loader = DataLoader(dataset=test_set, num_workers=4, pin_memory=True)
 
     # create model
-    model = DEEPCNN(in_channels=[1, 2, 4], out_channels=[2, 4, 8], kernel_size=[
-                    1, 2, 4], Hin=50, Win=200, hidden_dim=50, n_hidden=1, out_dim=5)
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    criterion = nn.BCELoss()
+    print('create model')
+    deepcnn = DEEPCNN(in_channels=[1], out_channels=[1], kernel_sizes=[3], strides=[
+                      3], paddings=[0], dilations=[1], Hin=50, Win=200, hidden_dim=40, n_hidden=2, out_dim=36)
+    optimizer = optim.Adam(deepcnn.parameters(), lr=lr)
+    criterion_bce = nn.BCELoss()
+    criterion_mse = nn.MSELoss()
     if USE_CUDA:
-        model = model.cuda()
+        deepcnn = deepcnn.cuda()
 '''
     # train
-    history = train_loop((train_loader, None), model,
-                         optimizer, criterion, epochs)
+    print('train')
+    history = train_loop((train_loader, test_loader), deepcnn,
+                         optimizer, (criterion_bce, criterion_mse), epochs)
 
     # evaluation
-    train_pred, train_acc = evaluation(train_loader, model, ohe)
+    print('evaluation')
+    train_pred, train_acc = evaluation(train_loader, deepcnn, ohe)
     print('Training dataset accuracy: {}'.format(round(np.mean(train_acc), 4)))
-    test_pred, test_acc = evaluation(test_loader, model, ohe)
+    test_pred, test_acc = evaluation(test_loader, deepcnn, ohe)
     print('Test dataset accuracy: {}'.format(round(np.mean(test_acc), 4)))
 '''
